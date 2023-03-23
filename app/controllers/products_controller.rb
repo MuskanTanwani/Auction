9a4a1f74fea797c1a3392146b605d8da45947f2e
muskanTanwani <mtanwani@gammastack.com>
2023-03-23 15:36:12 +0530

    currentchanges

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,16 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_product, only: [:edit, :destroy]
+
+  before_action :set_product, only: [:edit, :destroy, :update]
+
   def index
     @products = current_user.products.all
   end
+
   def new
     @product = current_user.products.new
   end
+
   def create
     @product = current_user.products.new(product_params)
     if @product.save
@@ -15,30 +19,35 @@ class ProductsController < ApplicationController
       render :new
     end
   end
+
   def edit
   end
+
   def update 
-    @product = Product.find_by(id: params[:id])
     if @product.update(product_params)
       redirect_to products_path, flash:{notice:"Item Data has been Updated"}
     else 
       render :edit, flash:{notice:"Item has Not been Updated. Please Verify Details"}
     end
   end
+
   def destroy 
     if @product.destroy
       redirect_to products_path, flash:{notice:"Item has been deleted.Thanks!"}
     end
   end
+
   private
+
   def set_product
-    if Product.find_by(id: params[:id]).nil?
+    @product = Product.find_by(id: params[:id])
+    unless @product.present?
       redirect_to products_path, flash:{notice:"Item does not exist"}
-    else
-      @product = Product.find_by(id: params[:id])
     end
   end
+
   def product_params
     params.require(:product).permit(:name,:your_name,:comapny_name,:description,:bank_account,:quantity,:starting_bid,:expected_bid,:phone_number)
   end
+
 end
